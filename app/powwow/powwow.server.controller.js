91abfcc7a2f2a80730ba25077e935166014c69a7
Rafa Garcia <rafa.gda.dev@gmail.com>
2015-08-21 14:51:42 +0200

    se vuelve a ordenar de manera descendente

--- app/powwow/powwow.server.controller.js
+++ app/powwow/powwow.server.controller.js
@@ -142,7 +142,7 @@ exports.list = function(req, res) {
 	if (req.query.userId){
 		Powwow.filter(function(powwow) {
    			return powwow('subscribers').contains(req.query.userId);
-		}).sortBy('time').run().then(function(powwows){
+		}).sortBy('time', true).run().then(function(powwows){
 			res.jsonp(powwows);
 		}).error(function(err){
 			return res.status(400).send({
@@ -151,7 +151,7 @@ exports.list = function(req, res) {
 		});
 	}
 
-	Powwow.sortBy('time').run().then(function(powwows) {
+	Powwow.sortBy('time', true).run().then(function(powwows) {
 			res.jsonp(powwows);
 		}).error(function (err){
 			return res.status(400).send({
